c67528841d3f32318b8baf5b27cfb9a4eb106e17
Lydiadev77 <72736162+Lydiadev77@users.noreply.github.com>
2020-11-12 17:24:27 +0530

    First commit

---
+++ antproj/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'antd/dist/antd.css';
+import reportWebVitals from './reportWebVitals';
+import { Layout,Input } from 'antd';
+
+const { Search } = Input;
+const { Header, Footer } = Layout;
+
+const onSearch = value => console.log(value);
+
+ReactDOM.render(
+  <>
+    <>
+    <Layout className="layout">
+    <Header>
+      <h4
+       className="title"
+       style={{ color: 'white'}}>Superhero Search</h4>
+    </Header>
+    <br></br>
+    <Search
+      placeholder="Search Superhero"
+      allowClear
+      onSearch={onSearch}
+      style={{ width: 'auto', margin: '0 10px', paddingLeft: '100px'}} 
+    />
+    {/* <Footer style={{ textAlign: 'center' }}>Search App Footer</Footer> */}
+  </Layout>,
+  </>,
+    
+    <br />
+    
+  </>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
