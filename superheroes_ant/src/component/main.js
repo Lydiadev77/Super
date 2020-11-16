dcd289846a674cdcad31748925dc53f3af1ec32a
Lydiadev77 <72736162+Lydiadev77@users.noreply.github.com>
2020-11-16 16:23:08 +0530

    second push

--- superheroes_ant/src/component/main.js
+++ superheroes_ant/src/component/main.js
@@ -5,14 +5,21 @@ import axios from "axios";
 const Main =()=>{
     const [data, setData] = useState();
     const [name, setName] = useState("");
-    //Characteristics of searched superhero
+
     const [combat, setCombat] = useState();
     const [durability, setDurability] = useState();
     const [intelligence, setIntelligence] = useState();
     const [power, setPower] = useState();
     const [speed, setSpeed] = useState();
     const [strength, setStrength] = useState();
-
+    const [fullname, setFullname] = useState();
+    const [gender, setGender] = useState();
+    const [race, setRace] = useState();
+    const [height, setHeight] = useState();
+    const [weight, setWeight] = useState();
+    const [occupation, setOccupation] = useState();
+    const [base, setBase] = useState();
+ 
     const[button, setButton] = useState("");
     const [img,setImg]=useState();
     const[id,setId] =useState();
@@ -22,12 +29,19 @@ const Main =()=>{
         .then((response) => {setData(response)
           setId(response.data.results[0].id)
            setImg(response.data.results[0].image.url)
+           setFullname(response.data.results[0].name)
            setCombat(response.data.results[0].powerstats.combat)
            setDurability(response.data.results[0].powerstats.durability)
            setIntelligence(response.data.results[0].powerstats.intelligence)
            setPower(response.data.results[0].powerstats.power)
            setSpeed(response.data.results[0].powerstats.speed)
            setStrength(response.data.results[0].powerstats.strength)
+           setGender(response.data.results[0].appearance.gender)
+           setRace(response.data.results[0].appearance.race)
+           setHeight(response.data.results[0].appearance.height)
+           setWeight(response.data.results[0].appearance.weight)
+           setOccupation(response.data.results[0].work.occupation)
+           setBase(response.data.results[0].work.base)
         })
         .catch((error) => console.log(error));        
     }, [button]);
@@ -42,13 +56,14 @@ return(
      <div className="searchbar">
   <input type="text" placeholder="Enter Name" value={name} onChange={ e => setName(e.target.value) } />
   <br/>
-  <button type="button" onClick={handleclick}>Search</button>
+  <button type="button" className="button" onClick={handleclick}>Search</button>
   </div>
   {button === "" ? <h1>.</h1> : <div className='fetch'>
     <img src={img}/>
     <div className="property">
-        <table>
-            <tr><th>Name: {name}</th></tr>
+    <h3 className="name"> Name: {fullname}</h3>
+        <table id="heroes">
+            
             <tr>
                 <td>Combat: {combat}</td>
                 <td>Durability: {durability}</td>
@@ -56,7 +71,18 @@ return(
                 <td>Power: {power}</td>
                 <td>Speed: {speed}</td>
                 <td>Strength: {strength}</td>
-            </tr></table>
+            </tr>
+            <tr>
+                <td> Gender: {gender}</td>
+                <td>Race: {race}</td>
+                <td>Height: {height}</td>
+                <td>Weight: {weight}</td>
+                <td colspan="2">Occupation: {occupation}</td>
+                
+            </tr>
+           
+            </table>
+           <h5><u>Work base:</u></h5> <p>{base}</p> 
     </div>
     
 </div>}
